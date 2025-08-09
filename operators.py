ad9d39f4b7c2be037cfe2c3ed0f2bc35d5ba57bf
MONIKA-49 <monikakumutha88@gmail.com>
2025-08-09 20:52:22 +0530

    Create operators.py
    initial commit

---
+++ operators.py
@@ -0,0 +1,20 @@
+# operators
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+while True:
+    choice=input("Operation Choice(1.Addition,2.Subtraction,3.Multiplication,4.Division):")
+    if choice=="1":
+        print("Result:",num1+num2)
+        break
+    elif choice =="2":
+        print("Result:",num1-num2)
+        break
+    elif choice=="3":
+       print("Result:",num1*num2)
+       break
+    elif choice=="4":
+        print("Result:",num1/num2)
+        break
+    else:
+        print("Invalid Choice")
+        break
